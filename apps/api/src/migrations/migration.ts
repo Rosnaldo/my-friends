b9e7211ed9436af91fabc32fe3bd3da9055d4830
Andrey Kenji Tsuzuki <andreytsuzuki@gmail.com>
2026-04-29 13:40:50 -0300

    migration fix

--- apps/api/src/migrations/migration.ts
+++ apps/api/src/migrations/migration.ts
@@ -2,18 +2,25 @@ import { getMigrationDao } from '#daos/singleton';
 import { RuntimeFiles } from '#utils/runtime_files';
 import path from 'path';
 
+
 export class Migration {
     public runScripts = async () => {
         const dir =  path.join(__dirname, 'scripts');
         const runtime = new RuntimeFiles();
-        const scripts = runtime.get(dir, __filename);
-        for await (const script of scripts) {
+        const files = runtime.get(dir, __filename);
+        for await (const fileName of files) {
+            const script = this.getScript(fileName);
             const migration = await getMigrationDao().findOne(script);
             if (!migration) {
-                const { migrate } = await import(script);
+                const { migrate } = await import(fileName);
                 await migrate.run();
                 await getMigrationDao().insert(script);
             }
         }
     };
+
+    private getScript = (fullName: string): string => {
+        const parts = fullName.trim().split('/');
+        return parts[parts.length - 1];
+    }
 }
